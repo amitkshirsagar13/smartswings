9a0a75522afa538ed98ebe18864983d9ee06c643
Amit Kshirsagar <amitkshirsagar13@gmail.com>
2020-06-19 21:02:25 +0530

    look and feel final

--- basepanel/src/main/java/io/smart/swings/utils/LookAndFeel.java
+++ basepanel/src/main/java/io/smart/swings/utils/LookAndFeel.java
@@ -14,21 +14,21 @@ public class LookAndFeel {
     private ComponentConfiguration componentConfiguration;
     public void lookAndFeel() {
         try {
-            UIManager.put("ProgressBar.verticalSize", new Dimension(400, 20));
-            if (StringUtils.isNotEmpty(componentConfiguration.getLookAndFeel()))
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                log.info(info.getName());
-                if (componentConfiguration.getLookAndFeel().equalsIgnoreCase(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    UIManager.put("nimbusOrange", new Color(40, 225, 40));
-                    UIManager.put("Table.background", Color.WHITE);
-                    UIManager.put("Table.alternateRowColor", Color.BLUE);
-                    UIManager.put("nimbusSelectionBackground", new Color(220, 220, 220));
-                    UIManager.put("List[Selected].textBackground", new Color(220, 220, 220));
-                    break;
+            if (StringUtils.isNotEmpty(componentConfiguration.getLookAndFeel()) && System.getProperty("os.name").contains("Mac OS")) {
+                for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                    if (componentConfiguration.getLookAndFeel().equalsIgnoreCase(info.getName())) {
+                        UIManager.setLookAndFeel(info.getClassName());
+                        UIManager.put("nimbusOrange", new Color(40, 225, 40));
+                        UIManager.put("Table.background", Color.WHITE);
+                        UIManager.put("Table.alternateRowColor", Color.BLUE);
+                        UIManager.put("nimbusSelectionBackground", new Color(220, 220, 220));
+                        UIManager.put("List[Selected].textBackground", new Color(220, 220, 220));
+                        break;
+                    }
                 }
             }
         } catch (Exception e) {
+            log.error("failed to load look and feel");
         }
     }
 }

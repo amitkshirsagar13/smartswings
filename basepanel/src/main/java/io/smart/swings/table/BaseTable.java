a425c3561d19d7b494be644abf09dbcd7936540a
poonamkshirsagar13 <poonam.kshirsagar.13@gmail.com>
2020-06-20 12:02:16 +0530

    fff

--- basepanel/src/main/java/io/smart/swings/table/BaseTable.java
+++ basepanel/src/main/java/io/smart/swings/table/BaseTable.java
@@ -1,6 +1,21 @@
 package io.smart.swings.table;
 
+import io.smart.swings.basemodel.SwingsModel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import javax.swing.*;
 
+@Data
+@RequiredArgsConstructor
 public class BaseTable extends JTable {
+    private final SwingsModel tableModel;
+    private ListSelectionModel select;
+
+
+    public void populateTable(JPanel contentPanel) {
+        setSelect(select);
+        JScrollPane sp=new JScrollPane(this);
+        contentPanel.add(sp);
+    }
 }
